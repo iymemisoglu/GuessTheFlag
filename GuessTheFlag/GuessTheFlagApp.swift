e671dd9d7f7ec54edab62a5493c45da8e5ba518b
ilker yasin memisoglu <iymemisoglu@gmail.com>
2025-08-30 12:29:38 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by İlker Memişoğlu on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
